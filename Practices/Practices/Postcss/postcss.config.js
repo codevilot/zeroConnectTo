4a8d5e4c31cc081f6edeb206d4cd631a47b896c1
codevilot <namhundred@naver.com>
2022-07-25 16:54:14 +0900

    Postcss 사용 예시

---
+++ Practices/Practices/Postcss/postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    // cssnano: {
+    // 
+    // },
+    "postcss-pxtorem": {
+
+    },
+    "postcss-import": {},
+    "postcss-combine-media-query": {},
+    "postcss-preset-env": {
+      "stage": 0,
+    }
+  }
+}
